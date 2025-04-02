1a62547c62d26d1d13aedb320939a609ecbf71fb
1reps <kdm9982@gmail.com>
2025-04-02 13:18:48 +0900

    수정

--- src/main/java/com/me/performance/domain/payment/PaymentService.java
+++ src/main/java/com/me/performance/domain/payment/PaymentService.java
@@ -9,19 +9,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final ReferenceNumberGenerator referenceNumberGenerator;
 
-    @Transactional(readOnly = true)
     public PaymentResponse read(Long paymentId) {
-        return paymentRepository.findById(paymentId)
-            .map(PaymentResponse::from)
+        return paymentRepository.findById(paymentId).map(PaymentResponse::from)
             .orElseThrow(() -> new IllegalArgumentException("Payment not found paymentId=" + paymentId));
     }
 
-    @Transactional(readOnly = true)
     public List<PaymentResponse> readAll() {
         return paymentRepository.findAll().stream()
             .map(PaymentResponse::from)
@@ -29,25 +27,23 @@ public class PaymentService {
     }
 
     public void deposit(Account account, BigDecimal amount) {
-        paymentRepository.save(
-            Payment.createDeposit(
-                referenceNumberGenerator.generateDepositReferenceNumber(),
-                account,
-                amount,
-                null
-            )
+        Payment newPayment = Payment.createDeposit(
+            referenceNumberGenerator.generateDepositReferenceNumber(),
+            account,
+            amount,
+            null
         );
+        paymentRepository.save(newPayment);
     }
 
     public void withdrawal(Account account, BigDecimal amount) {
-        paymentRepository.save(
-            Payment.createWithdrawal(
-                referenceNumberGenerator.generateWithdrawalReferenceNumber(),
-                account,
-                amount,
-                null
-            )
+        Payment newPayment = Payment.createWithdrawal(
+            referenceNumberGenerator.generateWithdrawalReferenceNumber(),
+            account,
+            amount,
+            null
         );
+        paymentRepository.save(newPayment);
     }
 
 }
